b42b2679136ebd902a661ddbaab2da82356bd0db
victorvieiradev <contatovictorvieira@outlook.com>
2022-08-24 20:23:31 -0300

    ajustes no model

--- src/main/java/com/mycompany/financas/model/ContasAPagarModel.java
+++ src/main/java/com/mycompany/financas/model/ContasAPagarModel.java
@@ -1,9 +1,23 @@
 package com.mycompany.financas.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contasapagar")
 public class ContasAPagarModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private double valor;
     private TipoEnum tipo;
@@ -11,4 +25,11 @@ public class ContasAPagarModel {
     private LocalDate dataDeVencimento;
     private LocalDateTime dataDePagamento;
 
+    public ContasAPagarModel(Long id, String nome, double valor, TipoEnum tipo, LocalDate dataDeVencimento) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.dataDeVencimento = dataDeVencimento;
+    }
 }
--- src/main/java/com/mycompany/financas/model/TipoEnum.java
+++ src/main/java/com/mycompany/financas/model/TipoEnum.java
@@ -1,6 +1,6 @@
 package com.mycompany.financas.model;
 
 public enum TipoEnum {
-    LUZ, AGUA, COMIDA, LAZER;
+    LUZ, AGUA, COMIDA, LAZER, OUTROS;
 
 }
