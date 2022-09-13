c91b5e087afc20d0339e7cbc3082a0bb9a1db723
victorvieiradev <contatovictorvieira@outlook.com>
2022-09-13 14:43:14 -0300

    ajustes

--- src/main/java/com/mycompany/financas/service/ReceitaService.java
+++ src/main/java/com/mycompany/financas/service/ReceitaService.java
@@ -1,4 +1,27 @@
 package com.mycompany.financas.service;
 
+import com.mycompany.financas.model.ReceitaModel;
+import com.mycompany.financas.repository.ReceitaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ReceitaService {
+    @Autowired
+    private ReceitaRepository receitaRepository;
+    public ReceitaModel salvar(ReceitaModel receitaModel){
+        return receitaRepository.save(receitaModel);
+    }
+    public List<ReceitaModel> exibir(){
+        return receitaRepository.findAll();
+    }
+    public Optional<ReceitaModel> buscarPorId(Integer id ){
+        return receitaRepository.findById(id);
+    }
+    public void excluir(ReceitaModel receitaModel ){
+        receitaRepository.delete(receitaModel);
+    }
 }
